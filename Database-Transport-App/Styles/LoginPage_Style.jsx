709b7e1ca4cee8dbd72fbed5cc40e32351f279ed
jun28th <doanlevy2806@gmail.com>
2024-12-12 01:24:09 +0700

    loginstyle

--- Database-Transport-App/Styles/LoginPage_Style.jsx
+++ Database-Transport-App/Styles/LoginPage_Style.jsx
@@ -4,9 +4,61 @@ import { Colors } from '../constants/Colors';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: '50%'
+    backgroundColor: '#FFFFE8',
   },
 
+  Header: {
+    marginTop: '50%',
+    marginLeft: '10%',
+  },
+  
+  Header_Text: {
+    fontSize: 24, 
+		fontFamily: 'roboto-bold', 
+		color: '#608BC1',
+  },
+
+  Describe: {
+    fontSize: 14,
+    fontFamily: 'roboto',
+    color:'#6E6868',
+  },
+  Content: {
+    marginLeft:'10%',
+    marginTop: '10%',
+  },
+
+  frame_to_fill: {
+    width: '90%',
+    height: 50,
+    marginTop:'2%',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 4,
+    marginBottom: 16,
+    paddingHorizontal: 10,
+    backgroundColor: '#fff',
+  },
+
+  Forgot_Pass: {
+    color:'#4A628A',
+    fontFamily:'roboto',
+  },
+
+  Login_Button: {
+    marginTop:'10%',
+    marginLeft:'20%',
+    marginRight:'20%',
+    backgroundColor: '#4A628A', 
+    paddingVertical: 12,
+    paddingHorizontal: 40,
+    borderRadius: 4,
+  },
+
+  Login_Button_Text: {
+    fontFamily: 'roboto-bold',
+    color: '#FFFFE8',
+    fontSize: 16,
+    textAlign: 'center',
+  }
 });
