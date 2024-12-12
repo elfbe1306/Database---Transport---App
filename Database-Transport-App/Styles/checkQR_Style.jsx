40077503a09eb6f7a90a167003bf709244e49402
jun28th <doanlevy2806@gmail.com>
2024-12-12 18:24:56 +0700

    CheckQR frontend

---
+++ Database-Transport-App/Styles/checkQR_Style.jsx
@@ -0,0 +1,130 @@
+import { StyleSheet } from "react-native";
+import { Colors } from '../constants/Colors';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFE8'
+    },
+    
+    headerContainer: {
+        flex: 2.5
+    },
+    bodyContainer: {
+        flex: 6
+    },
+    
+    Footer_container: {
+        flex: 3.5
+    },
+    
+    firstBody: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginHorizontal: '10%',
+        marginVertical:'5%',
+      },
+    
+      firstHeaderContainer: {
+        position: 'absolute',
+        marginTop: '20%',
+        justifyContent:'space-between=',
+        alignItems: 'center',
+        alignContent: 'center',
+        gap: '35%',
+        flexDirection:'row',
+      },
+      userNameBox: {
+        backgroundColor: '#4A628A',
+        paddingLeft: 5,
+        paddingRight: 30,
+        paddingVertical: 5,
+        borderRadius: 40,
+        marginLeft: '5%',
+        flexDirection:'row',
+      },
+      imageBox: {
+        width: 50,  
+        height: 50, 
+        borderRadius: 75, 
+        overflow: 'hidden', 
+      },
+      userImage : {
+        top: -13,
+        left: 0,
+        width: 50, 
+        height: 75,
+        resizeMode: 'contain', 
+      },
+      userName: {
+        marginTop: 16,
+        marginLeft: 15,
+        fontFamily: 'roboto-bold',
+        color:'#FFFFE8',
+        fontSize: 16 
+      },
+      
+      notificationButton: {
+        padding: 15,
+        backgroundColor: 'white',
+        borderRadius: 99
+      },
+      tableContainer: {
+        paddingHorizontal: 10,
+        marginTop: 30,
+      },
+      table: {
+        borderWidth: 1,
+        borderColor: 'black',
+        width: '100%',
+      },
+      row: {
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderColor: 'black',
+      },
+    
+      row_Header: {
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderColor: 'black',
+        backgroundColor:'#4A628A'
+      },
+    
+      cell_Header: {
+        flex: 1,
+        textAlign: 'center',
+        padding: 10,
+        borderRightWidth: 1,
+        borderColor: 'black',
+        fontSize: 12,
+        fontFamily: 'roboto',
+        color: '#FFFFE8'
+      },
+    
+      cell_Data: {
+        flex: 1,
+        textAlign: 'center',
+        padding: 10,
+        borderRightWidth: 1,
+        borderColor: 'black',
+        fontSize: 11,
+        fontFamily: 'roboto',
+      },
+    
+      confirmed_Button: {
+        marginLeft:'60%',
+        marginRight:'14%',
+        backgroundColor: '#4A628A', 
+        paddingVertical: 10,
+        borderRadius: 12,
+      },
+    
+      confirmed_Button_Text: {
+        fontFamily: 'roboto-bold',
+        color: '#FFFFE8',
+        fontSize: 12,
+        textAlign: 'center',
+      }
+      
+})
--- Database-Transport-App/app/(tabs)/checkQR.jsx
+++ Database-Transport-App/app/(tabs)/checkQR.jsx
@@ -1,8 +1,63 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView } from 'react-native'
+import { styles } from '../../Styles/checkQR_Style' 
+import Feather from '@expo/vector-icons/Feather';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function checkQR() {
   return (
-    <View><Text>checkQR</Text></View>
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <View style={styles.firstHeaderContainer}>
+          <View style={styles.userNameBox}>
+            <View style={styles.imageBox}>
+            <Image source={{ uri: 'https://i.pinimg.com/736x/64/56/c4/6456c41df52ca456a072dc086c091f7a.jpg' }} style={styles.userImage}/>
+            </View>
+            <Text style={styles.userName}>Doan Le Vy</Text>
+          </View>
+
+          <View style={styles.notificationButton}>
+            <Feather name="bell" size={30} color="#4A628A" />
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.bodyContainer}> 
+        <ScrollView style={styles.tableContainer}>
+        <View style={styles.table}>
+          {/* Table Header */}
+          <View style={styles.row_Header}>
+            <Text style={[styles.cell_Header, { flex: 1.2 }]}>ID</Text>
+            <Text style={[styles.cell_Header, { flex: 4}]}>Product</Text>
+            <Text style={[styles.cell_Header, { flex: 1.2 }]}>ScanQR</Text>
+            <Text style={[styles.cell_Header, { flex: 1.2}]}>Status</Text>
+          </View>
+          
+          {/* Table Rows */}
+          <View style={styles.row}>
+            <Text style={[styles.cell_Data, { flex: 1.2 }]}>MP0001</Text>
+            <Text style={[styles.cell_Data, { flex: 4 }]}>Sữa Rửa Mặt CeraVe Sạch Sâu Cho Da Thường Đến Da Dầu 473ml</Text>
+            <Text style={[styles.cell_Data, { flex: 1.2 }]}><MaterialIcons name="qr-code-scanner" size={24} color="black" /></Text>
+            <Text style={[styles.cell_Data, { flex: 1.2 }]}>success</Text>
+          </View>
+
+          <View style={styles.row}>
+            <Text style={[styles.cell_Data, { flex: 1.2 }]}>MP0002</Text>
+            <Text style={[styles.cell_Data, { flex: 4 }]}>Kem Chống Nắng La Roche-Posay Kiểm Soát Dầu SPF50+ 50ml</Text>
+            <Text style={[styles.cell_Data, { flex: 1.2 }]}><MaterialIcons name="qr-code-scanner" size={24} color="black" /></Text>
+            <Text style={[styles.cell_Data, { flex: 1.2 }]}>not started</Text>
+          </View>
+      
+        </View>
+        </ScrollView>
+      </View>
+
+      <View style={styles.Footer_container}>
+          <TouchableOpacity style={styles.confirmed_Button}>
+            <Text style={styles.confirmed_Button_Text}>Start Delivery</Text>
+        </TouchableOpacity>
+        </View>
+
+    </View>
   )
 }
