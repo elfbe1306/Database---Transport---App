193c4ebf828f2a70e7ef8d52a34d7e70e726ac65
jun28th <doanlevy2806@gmail.com>
2024-12-12 14:47:18 +0700

    home

--- Database-Transport-App/Styles/HomePage_Style.jsx
+++ Database-Transport-App/Styles/HomePage_Style.jsx
@@ -4,7 +4,33 @@ import { Colors } from '../constants/Colors';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems:'center'
+    backgroundColor: '#FFFFE8'
   },
+
+  headerContainer: {
+    flex: 2.5
+  },
+  bodyContainer: {
+    flex: 7.5
+  },
+
+  firstBody: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: '10%',
+    marginVertical:'5%',
+  },
+
+  set_Day_Time: {
+    fontSize: 16,
+    fontFamily:'roboto-bold',
+    color: '#4A628A',
+    marginHorizontal: 0,
+    textAlign: 'left',
+    marginLeft:'10%',
+    marginTop:'45%'
+  },
+
+
+  
 });
--- Database-Transport-App/app/(tabs)/home.jsx
+++ Database-Transport-App/app/(tabs)/home.jsx
@@ -1,11 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView } from 'react-native'
 import React from 'react'
 import {styles} from '../../Styles/HomePage_Style'
+import { format } from 'date-fns'; 
 
 export default function home() {
+  const today = format(new Date(), 'EEEE MMMM d, yyyy');
+
+  const data = [
+    ['Today Task', 'Branch Name', 'Report', 'Status'], // Header row
+    ['Export', 'Kho Quận 1', 'view', 'not ready'],
+    ['Retrieve','Kho Quận Phú Nhuận','view','in progress']
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>THANK YOU LEVYYYYY</Text>
+      <View style={styles.headerContainer}>
+        <View>
+          <Text style={styles.set_Day_Time}>{today}</Text>
+        </View>
+      </View>
+
+      <View style={styles.bodyContainer}> 
+        
+      </View>
     </View>
   )
 }
