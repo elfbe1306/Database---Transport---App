c10ec2232ef6ca970248683eae68ad205521d228
jun28th <doanlevy2806@gmail.com>
2024-12-12 16:28:46 +0700

    home

--- Database-Transport-App/Styles/HomePage_Style.jsx
+++ Database-Transport-App/Styles/HomePage_Style.jsx
@@ -21,6 +21,46 @@ export const styles = StyleSheet.create({
     marginVertical:'5%',
   },
 
+  firstHeaderContainer: {
+    position: 'absolute',
+    marginTop: '15%',
+    justifyContent:'space-between=',
+    alignItems: 'center',
+    alignContent: 'center',
+    gap: '38%',
+    flexDirection:'row',
+  },
+  userNameBox: {
+    backgroundColor: 'white',
+    paddingLeft: 5,
+    paddingRight: 30,
+    paddingVertical: 5,
+    borderRadius: 40,
+    marginLeft: '5%',
+    flexDirection:'row',
+  },
+  imageBox: {
+    backgroundColor: 'white', 
+    borderRadius: 99,
+    height: 50,
+  },
+  userImage : {
+    top: 0.5,
+    left: 0,
+    width: 50, 
+    height: 75,
+  },
+  userName: {
+    marginTop: 16,
+    marginLeft: 15,
+    fontFamily: 'roboto'
+  },
+  notificationButton: {
+    padding: 15,
+    backgroundColor: 'white',
+    borderRadius: 99
+  },
+
   set_Day_Time: {
     fontSize: 16,
     fontFamily:'roboto-bold',
@@ -31,6 +71,46 @@ export const styles = StyleSheet.create({
     marginTop:'45%'
   },
 
+  tableContainer: {
+    paddingHorizontal: 10,
+  },
+  table: {
+    borderWidth: 1,
+    borderColor: 'black',
+    width: '100%',
+  },
+  row: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderColor: 'black',
+  },
 
+  row_Header: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderColor: 'black',
+    backgroundColor:'#4A628A'
+  },
+
+  cell_Header: {
+    flex: 1,
+    textAlign: 'center',
+    padding: 10,
+    borderRightWidth: 1,
+    borderColor: 'black',
+    fontSize: 12,
+    fontFamily: 'roboto',
+    color: '#FFFFE8'
+  },
+
+  cell_Data: {
+    flex: 1,
+    textAlign: 'center',
+    padding: 10,
+    borderRightWidth: 1,
+    borderColor: 'black',
+    fontSize: 11,
+    fontFamily: 'roboto',
+  },
   
 });
--- Database-Transport-App/app/(tabs)/home.jsx
+++ Database-Transport-App/app/(tabs)/home.jsx
@@ -2,26 +2,62 @@ import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView } from 'rea
 import React from 'react'
 import {styles} from '../../Styles/HomePage_Style'
 import { format } from 'date-fns'; 
+import Feather from '@expo/vector-icons/Feather';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function home() {
   const today = format(new Date(), 'EEEE MMMM d, yyyy');
 
-  const data = [
-    ['Today Task', 'Branch Name', 'Report', 'Status'], // Header row
-    ['Export', 'Kho Quận 1', 'view', 'not ready'],
-    ['Retrieve','Kho Quận Phú Nhuận','view','in progress']
-  ];
-
   return (
     <View style={styles.container}>
+
       <View style={styles.headerContainer}>
+        <View style={styles.firstHeaderContainer}>
+          <View style={styles.userNameBox}>
+            <View style={styles.imageBox}>
+            <Ionicons name="person-circle-outline" size={50} color="#4A628A" style={styles.userImage}/>
+            </View>
+            <Text style={styles.userName}>Doan Le Vy</Text>
+          </View>
+
+          <View style={styles.notificationButton}>
+            <Feather name="bell" size={30} color="#4A628A" />
+          </View>
+        </View>
+
         <View>
           <Text style={styles.set_Day_Time}>{today}</Text>
         </View>
       </View>
 
       <View style={styles.bodyContainer}> 
-        
+        <ScrollView style={styles.tableContainer}>
+        <View style={styles.table}>
+          {/* Table Header */}
+          <View style={styles.row_Header}>
+            <Text style={styles.cell_Header}>Today's Task</Text>
+            <Text style={styles.cell_Header}>Branch's Name</Text>
+            <Text style={styles.cell_Header}>Report</Text>
+            <Text style={styles.cell_Header}>Status</Text>
+          </View>
+          
+          {/* Table Rows */}
+          <View style={styles.row}>
+            <Text style={styles.cell_Data}>Export</Text>
+            <Text style={styles.cell_Data}>Kho Quận 1</Text>
+            <Text style={styles.cell_Data}>RP0001</Text>
+            <Text style={styles.cell_Data}>not started</Text>
+          </View>
+
+          <View style={styles.row}>
+            <Text style={styles.cell_Data}>Retrieve</Text>
+            <Text style={styles.cell_Data}>Kho Quận Phú Nhuận</Text>
+            <Text style={styles.cell_Data}>RT0001</Text>
+            <Text style={styles.cell_Data}>in progress</Text>
+          </View>
+      
+        </View>
+        </ScrollView>
       </View>
     </View>
   )
